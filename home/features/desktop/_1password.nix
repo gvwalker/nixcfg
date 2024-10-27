43c77524b6a9d20f378df4159762a45f2610b62f
Grant Walker <grant.v.walker@gmail.com>
2024-10-27 12:18:21 +0200

    Added silent autostart to 1password

--- home/features/desktop/_1password.nix
+++ home/features/desktop/_1password.nix
@@ -15,5 +15,21 @@ in
     home.packages = with pkgs; [
       _1password-gui
     ];
+
+    home.file = {
+      ".config/autostart/1password.desktop".text = ''
+        [Desktop Entry]
+        Categories=Office;
+        Comment=Password manager and secure wallet
+        Exec=1password --silent %U
+        Icon=1password
+        Name=1Password
+        StartupNotify=true
+        StartupWMClass=1Password
+        Terminal=false
+        Type=Application
+        X-KDE-SubstituteUID=false
+      '';
+    };
   };
 }
